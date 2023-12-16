56f3d996fde73100c001985bf860c38be4f34316
Mueez Ahmad <hello@mueez.me>
2023-12-16 04:00:50 -0600

    testing if site updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
+import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
-
-import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+    site: 'https://moooease.github.io',
+    integrations: [tailwind()]
 });
